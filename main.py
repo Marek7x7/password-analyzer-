0cd49cac12441922354276e5cf1ecc7a79b2a172
Marek Frydrychva <marekfrydrychova@gmail.com>
2026-01-19 20:51:14 -0500

    rules have been put in place

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-print("hello world")
-print("i LIIKE MAN")
+#If you think this sounds boring, good. Boring projects force clean logic. This one teaches strings, functions, loops, dictionaries, file handling, and basic security thinking.
+#Takes a password from the user
+
+#Analyzes its strength
+
+#Explains why it's weak or strong
+
+#Suggests concrete improvements
+
+#Your program must check:
+
+#Length
+
+#Uppercase letters
+
+#Lowercase letters
+
+#Numbers
+#Special characters
+
+#Repeated characters
+
+#Common patterns (like 123, password, qwerty)
